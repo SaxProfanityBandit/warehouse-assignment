1223d3da936275354e0f641fb89377bce85ed6a1
SaxProfanityBandit <rsahlgren@gmail.com>
2022-02-15 12:38:15 +0100

    Placeholder functions.

--- warehouse.py
+++ warehouse.py
@@ -6,3 +6,24 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/products")
+def get_products():
+    pass
+
+app.route("/products")
+def post_products():
+    pass
+
+app.route("/products/1")
+def get_product_1():
+    pass
+
+app.route("/products/1")
+def put_products():
+    pass
+
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
