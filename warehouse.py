e4299dc6f2cbc04f6f75b4426ecb8f820162218e
Rickard Sahlgren <rsahlgren@gmail.com>
2022-02-16 12:01:15 +0100

    Progress on connecting to the DB and JSON related stuff

--- warehouse.py
+++ warehouse.py
@@ -1,11 +1,32 @@
 #Imports
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+import json
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = '127.0.0.1'
+app.config['MYSQL_USER'] = 'warehouse_admin'
+app.config['MYSQL_PASSWORD'] = 'devops'
+app.config['MYSQL_DB'] = 'warehouse'
+
+mysql = MySQL(app)
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM products;")
+    mysql.connection.commit()
+    row = cur.fetchall()
+    desc = list(zip(*cur.description))[0]
+    rowdict = dict(zip(str(row[0]), desc))
+    jsondict = jsonify(rowdict)
+    cur.close()
+    output = ""
+    #for x in rows[1]:
+        #output = output + str(x)
+    return jsondict
 
 @app.route("/products")
 def get_products():
