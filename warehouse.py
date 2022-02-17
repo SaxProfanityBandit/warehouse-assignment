3457c3442e17a81c65e3852fd86310ed76fdcc5d
SaxProfanityBandit <rsahlgren@gmail.com>
2022-02-17 19:16:00 +0100

    Started on staff route.

--- warehouse.py
+++ warehouse.py
@@ -111,9 +111,46 @@ def customer(_id):
         db.commit()
         return make_response("Customer with ID {} updated.".format(_id), 200)
 
-@app.route("/staff", methods=['GET'])
+@app.route("/staff", methods=['GET', 'POST'])
 def get_staff():
-    return get_json("staff")
+    if request.method == 'GET':
+        return make_response(get_json("staff"), 200)
+    elif request.method == 'POST':
+        json_data = request.json
+        if json_data is not None:
+            cursor = db.cursor(dictionary=True)
+            query = (
+                "INSERT INTO staff (first_name, last_name, street, postal_code, age) "
+                "VALUES (%s, %s, %s, %s, %s);"
+            )
+            data = (json_data['first_name'], json_data['last_name'], json_data['street'], json_data['postal_code'], json_data['age'])
+            cursor.execute(query, data)
+            db.commit()
+            if cursor is not None:
+                result = cursor.fetchone()
+                print(result)
+            return make_response("Added new customer.", 201)
+
+    return make_response("Wrong type of request.", 400)
+
+@app.route("/staff/<int:_id>", methods=['GET', 'DELETE', 'PUT'])
+def staff(_id):
+    if request.method == 'GET':
+        cursor = db.cursor(dictionary=True)
+        cursor.execute('SELECT * FROM customers WHERE Id={};'.format(_id))
+        result = cursor.fetchall()
+        return make_response(jsonify(result), 200)
+    elif request.method == 'DELETE':
+        cursor = db.cursor(dictionary=True)
+        cursor.execute('DELETE FROM customers WHERE Id={};'.format(_id))
+        db.commit()
+        return make_response("Customer with ID {} deleted.".format(_id), 200)
+    elif request.method == 'PUT':
+        cursor = db.cursor(dictionary=True)
+        json_data = request.json
+        cursor.execute('UPDATE customers SET age={} WHERE Id={};'.format(json_data['age'], _id))
+        db.commit()
+        return make_response("Customer with ID {} updated.".format(_id), 200)
 
 @app.route("/orders", methods=['GET'])
 def get_orders():
@@ -125,17 +162,6 @@ def get_json(table):
     result = cursor.fetchall()
     return jsonify(result)
 
-app.route("/products")
-def post_products():
-    pass
-
-app.route("/products/1")
-def get_product_1():
-    pass
-
-app.route("/products/1")
-def put_products():
-    pass
 
 
 
